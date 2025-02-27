bb9898eef15d7f7aa336a47ede9b8557e0afdd1d
Nawang Dorjee <154888348+Reizhea@users.noreply.github.com>
2025-02-27 18:26:45 +0530

    Delete backend/routes/alertRoutes.js

--- backend/routes/alertRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const alertController = require("../controllers/alertController");
-
-router.post("/alerts", alertController.logAlert);
-router.get("/alerts/all", alertController.getAlerts);
-
-module.exports = router;
